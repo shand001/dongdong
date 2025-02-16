c0e80976daa99b1750fa69f3f727cadfe0342f30
shand001 <shenhandong01@gmail.com>
2025-02-16 17:53:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// 获取按钮和标题
+const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
+const question = document.getElementById('question');
+
+// 点击“可以”按钮
+yesButton.addEventListener('click', () => {
+    question.innerText = "太棒了！我也喜欢你 ❤️";
+});
+
+// 点击“不要”按钮
+noButton.addEventListener('mouseover', () => {
+    const x = Math.random() * window.innerWidth * 0.8;
+    const y = Math.random() * window.innerHeight * 0.8;
+    noButton.style.position = 'absolute';
+    noButton.style.left = `${x}px`;
+    noButton.style.top = `${y}px`;
+});
